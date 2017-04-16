5252932b9574dba602b4716d24076592e9e8fddc
aviadshiber <aviadshiber@campus.technion.ac.il>
2017-04-16 20:40:07 +0300

    changing the name of the class

--- README.md
+++ README.md
@@ -3,23 +3,23 @@ this repo is my first repo.
 in the following repo I will examine some small programs I wrote in Python. 
 I will try to examin some of the power in the python language and how it can be applied in the real world.
 
-## Polynom
+## LazyPolynom
 <p>
  the module will represent a Polynom with simple operations like add,multiply and compose of other polynoms in time complexity of O(1), then it will be possible to evaluate the polynom in time complexity of O(n). 
     all this will be fairly easy and possible using lambda expressions & closures features in python :).
  </p>
 
 ### example of usage: </br>
- p1= Polynom()</br>
- p2= Polynom()</br>
+ p1= LazyPolynom()</br>
+ p2= LazyPolynom()</br>
  p1.add_monom(2,1)</br>
 p2.add_monom(1,2).add_monom(5,0)</br>
 p1.add(p2.get_block()) # p1(x)= 2x+x^2+5</br>
 print p1.eval(2) #print 13</br>
-p3=Polynom()</br>
+p3=LazyPolynom()</br>
 p3.add_monom(5,2)</br>
 p1.multiply(p3.get_block()) #p1(x) = 5x^2 *(2x+x^2+5)</br>
 print p1.eval(2) # print 260</br>
-p4=Polynom().add_monom(1,2)</br>
+p4=LazyPolynom().add_monom(1,2)</br>
 p1.compose(p4.get_block()) #p1(x)= 5(x^2)^2 * (2(x^2)+(x^2)^2+5)</br>
 print p1.eval(2) # print 2320 </br>
--- pypolynom.py
+++ pypolynom.py
@@ -1,7 +1,8 @@
-class Polynom(object):
+class LazyPolynom(object):
     """
-    the module will represent a Polynom with simple operations like add,multiply and compose of other polynoms in time complexity of O(1), then it will be possible to evaluate the polynom in time complexity of O(n). 
+    the LazyPolynom module will represent a Polynom with simple operations like add,multiply and compose of other polynoms in time complexity of O(1), then it will be possible to evaluate the polynom in time complexity of O(n). 
     all this will be fairly easy and possible using lambda expressions & closures features in python :).
+    it is lazy because it postpone the need to evaluate & manipulate the polynom in order to do manipulations on it.
     """
     def __init__(self):
         self.block = (lambda x: 0)
@@ -71,19 +72,21 @@ class Polynom(object):
 
 
 #example of usage:
-p1= Polynom()
-p2= Polynom()
+'''
+p1= LazyPolynom()
+p2= LazyPolynom()
 p1.add_monom(2,1)
 p2.add_monom(1,2).add_monom(5,0)
 p1.add(p2.get_block()) # p1(x)= 2x+x^2+5
 print p1.eval(2) #print 13
-p3=Polynom()
+p3=LazyPolynom()
 p3.add_monom(5,2)
 p1.multiply(p3.get_block()) #p1(x) = 5x^2 *(2x+x^2+5)
 print p1.eval(2) # print 260
-p4=Polynom().add_monom(1,2)
+p4=LazyPolynom().add_monom(1,2)
 p1.compose(p4.get_block()) #p1(x)= 5(x^2)^2 * (2(x^2)+(x^2)^2+5)
 print p1.eval(2) # print 2320
+'''
 
 
 
